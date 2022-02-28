81c557351b5f21da48976fd1ef8b7e1ade37390d
dr96mabuza <dr96mabuza@gmail.com>
2022-02-28 11:28:36 +0200

    works, only page stying remaining

---
+++ EAS.js
@@ -0,0 +1,43 @@
+function grid(num) {
+    const content = document.querySelector('#container');
+    for (let i = 0; i < num; i++) {
+
+        const span = document.createElement('span');
+        content.appendChild(span);
+
+        for (let j = 0; j < num; j++){
+            
+            const div = document.createElement('div');
+            div.classList.add('grid');
+            div.addEventListener('click', () => {
+                div.style.cssText = 'background-color: black';
+            });
+            span.appendChild(div);
+
+        }
+    }
+
+    
+    
+}
+
+grid(16);
+
+function newGrid() {
+    document
+    .querySelectorAll('span')
+    .forEach((e) => e.parentNode.removeChild(e));
+    
+    let result = prompt('Enter number of squares you want: ');
+    if (result < 100 || result > 0){
+        return result;
+    } else {
+        return result = 16;
+    }
+    
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    grid(newGrid());
+});
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-A-Sketch
+Create a webpage with a 16x16 grid of square divs.
